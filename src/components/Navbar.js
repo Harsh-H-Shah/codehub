111bc3696c2743415043a4ee6d25bc8c292708e9
harsh <harshmkvviv@gmail.com>
2021-09-18 02:01:57 +0530

    Navbar background added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Logo from '../static/logos/LogoWhite.svg'
 
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar is lovely</h1>
+      <div className='w-screen flex flex-col h-16 shadow-2xl bg-redness bg-no-repeat'>
+        <img src={Logo} alt='logo' className='mix-blend-screen h-12'/>
+      </div>
     </div>
   )
 }
