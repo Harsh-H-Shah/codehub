eebf458d689a462e4ef5882451920f5a5d42d275
harsh <harshmkvviv@gmail.com>
2021-09-19 02:37:18 +0530

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Logo from '../static/logos/LogoWhite.svg'
+import AccLogo from '../static/Navbar-icons/account_circle.svg'
+import Search from '../static/Navbar-icons/search.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      <div className='w-screen flex flex-col h-16 shadow-2xl bg-redness bg-no-repeat'>
-        <img src={Logo} alt='logo' className='mix-blend-screen h-12'/>
-      </div>
-    </div>
+      <nav className='w-screen grid grid-cols-9 col-start-2 h-20 shadow-3xl bg-secondary-red justify-center items-center overscroll-none'>
+        <img src={Logo} alt='logo' className=' justify-self-start h-16'></img>
+        <div className='flex flex-row text-primary col-start-3'>
+          <p className='font-sans font-normal text-xl mr-10 cursor-pointer'>Courses</p>
+          <p className='font-sans font-normal text-xl mr-10 cursor-pointer'>Roadmap</p>
+          <p className='font-sans font-normal text-xl mr-10 cursor-pointer'>Quizzes</p>
+          <p className='font-sans font-normal text-xl mr-10 cursor-pointer'>Books</p>
+        </div>
+        <div className='w-64 h-8 bg-secondary-lightred rounded-md flex items-center place-content-between col-start-7'>
+          <input type='text' placeholder='Search a course' className='font-sans text-sm placeholder-primary px-4 bg-secondary-lightred focus:outline-none text-primary'/>
+          <img src={Search} alt='Search' className='mr-2' />
+        </div>
+        <img src={AccLogo} alt='Acc-logo' className='ml-8 col-start-9' />
+      </nav>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: {
-        'redness':"url('/src/static/Color-palette/Red.png')"
+    fontFamily: {
+      serif: 'Ibarra Real Nova',
+      sans: 'Montserrat'
+    },
+    colors:{
+      primary: "#FAFAFA",
+      secondary:{
+        lightgray: "#3A4750",
+        darkgray:"#303841",
+        red:"#F24D5D",
+        lightred:"#FF6E7C"
       }
     },
+    boxShadow: {
+      "2xl": '0 -20px 75px -12px rgba(0, 0, 0, 0.15)',
+      "3xl": '0 25px 50px 0px rgba(0, 0, 0, 0.25)',
+    }
   },
   variants: {
     extend: {},
