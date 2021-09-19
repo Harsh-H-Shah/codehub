fc3c40cb44a1a4cd02fc4584393bff3c8427e346
harsh <harshmkvviv@gmail.com>
2021-09-20 02:49:17 +0530

    Add routing and Book list component and Books page

---
+++ src/components/BookList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const BookList = () => {
+  return (
+    <div className="flex flex-col bg-primary rounded-lg justify-center w-3/4 mt-16 z-20 shadow-pn mb-32">
+      <div className="flex flex-row place-content-between border-b border-secondary-lightgray w-full h-32 border-opacity-10 items-center">
+        <p className='ml-8 text-2xl font-normal'>You dont know JS</p>
+        <button className="w-52 text-2xl h-12 mr-8 bg-secondary-red items-center rounded-md font-medium shadow-md text-primary">
+          Read now
+        </button>
+      </div>
+      <div className="flex flex-row place-content-between border-b border-secondary-lightgray w-full h-32 border-opacity-10 items-center">
+        <p className='ml-8 text-2xl font-normal'>You dont know JS</p>
+        <button className="w-52 text-2xl h-12 mr-8 bg-secondary-red items-center rounded-md font-medium shadow-md text-primary">
+          Read now
+        </button>
+      </div>
+      <div className="flex flex-row place-content-between border-b border-secondary-lightgray w-full h-32 border-opacity-10 items-center">
+        <p className='ml-8 text-2xl font-normal'>You dont know JS</p>
+        <button className="w-52 text-2xl h-12 mr-8 bg-secondary-red items-center rounded-md font-medium shadow-md text-primary">
+          Read now
+        </button>
+      </div>
+      <div className="flex flex-row place-content-between border-b border-secondary-lightgray w-full h-32 border-opacity-10 items-center">
+        <p className='ml-8 text-2xl font-normal'>You dont know JS</p>
+        <button className="w-52 text-2xl h-12 mr-8 bg-secondary-red items-center rounded-md font-medium shadow-md text-primary">
+          Read now
+        </button>
+      </div>
+      <div className="flex flex-row place-content-between border-b border-secondary-lightgray w-full h-32 border-opacity-10 items-center">
+        <p className='ml-8 text-2xl font-normal'>You dont know JS</p>
+        <button className="w-52 text-2xl h-12 mr-8 bg-secondary-red items-center rounded-md font-medium shadow-md text-primary">
+          Read now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BookList;
--- src/pages/CoursesPage.js
+++ src/pages/CoursesPage.js
@@ -27,4 +27,4 @@ const CoursesPage = () => {
     )
 }
 
-export default CoursesPage
+export default CoursesPage;
