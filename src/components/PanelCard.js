6fcb117cf6f337e21a32bb6591aff9e92fcbfe30
gayatrivjoshi <gayatrij768@gmail.com>
2021-09-19 15:01:46 +0530

    Add panel card

--- src/components/PanelCard.js
+++ src/components/PanelCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import CourseLogo from '../static/panel-logo/Courses.svg'
 
 const PanelCard = () => {
   return (
-    <div>
-      <h1>Panel Card</h1>
+    <div className=' w-32 h-32 bg-secondary-lightred flex flex-col place-content-evenly place-items-center rounded-md shadow-md'>
+      <img src={CourseLogo} alt="courselogo" className="w-16 h-16" />
+      <div className='text-primary text-xl '>Courses</div>
     </div>
   )
 }
